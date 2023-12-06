d3fa2c053bc949209e07c7bee9252bc71704ab23
JLowe32 <122652186+JLowe32@users.noreply.github.com>
2023-12-05 22:30:46 -0800

    Update player.h

--- player.h
+++ player.h
@@ -4,6 +4,16 @@
 
 #include <GL/freeglut.h>
 
+void drawBullets();
 void drawPlayer();
+
+struct Bullet {
+    float x, y;     // Bullet Position
+    bool active;    // Bullet Activision Check
+};
+
+void updateBulletPosition(Bullet& bullet, float bulletVelocityX, float bulletVelocityY);
+void bulletMoving(Bullet& bullet);
+void drawBullet(Bullet& bullet);
 
 #endif 
